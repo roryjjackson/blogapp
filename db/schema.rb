c86e41a9ca07acd3e4eaf3e6834ab8839226b7a5
rory jackson <roryjamesjackson@hotmail.com>
2022-11-04 13:20:40 +0000

    added crud functions for all models category, article and comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_163507) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_102124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,10 +41,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_163507) do
     t.string "title"
     t.text "content"
     t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
     t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["category_id"], name: "index_comments_on_category_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -67,5 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_163507) do
   add_foreign_key "articles", "categories"
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "categories"
   add_foreign_key "comments", "users"
 end
