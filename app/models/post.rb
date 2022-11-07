36b56f68422156a442c972d0f1e78585414bcce2
rory jackson <roryjamesjackson@hotmail.com>
2022-11-07 14:03:03 +0000

    searchbar searching through post titles

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,21 @@ class Post < ApplicationRecord
   def liked?(user)
     !!self.likes.find{|like| like.user_id == user_id}
   end
+
+  # include PgSearch::module pg_search_scope, :search_by_title_summary_and_category,
+  #   against: %i[ title summary category ],
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
+
+  # pg_search_scope :global_search,
+  #   against: %i[ title summary ],
+  #   associated_against: {
+  #     user: %i[ first_name last_name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
+
+  # end
 end
