e77248e0b73c44cca36f682cf093aaf1a06ee237
rory jackson <roryjamesjackson@hotmail.com>
2022-11-08 12:07:33 +0000

    added conditional logic throughout the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit::Authorization
+
+  after_action  :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name author admin])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name author admin])
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
---
+++ app/policies/post_policy.rb
@@ -0,0 +1,28 @@
+class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+    def resolve
+      scope.all
+      user.admin? ? scope.all : scope.where(user: user)
+    end
+  end
+
+  def show
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
